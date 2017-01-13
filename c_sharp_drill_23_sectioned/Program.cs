f305c667ca7839761fd19bd8b9f40c6443617aec
Tranel Bland <tranelb@gmail.com>
2017-01-13 10:41:33 -0800

    Add project files.

---
+++ c_sharp_drill_23_sectioned/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_drill_23_sectioned
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
