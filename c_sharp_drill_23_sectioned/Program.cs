212a952a48223fe9fe094cf52730953a8e6643ab
Tranel Bland <tranelb@gmail.com>
2017-01-13 11:46:24 -0800

    added more objects

--- c_sharp_drill_23_sectioned/Program.cs
+++ c_sharp_drill_23_sectioned/Program.cs
@@ -4,6 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+public enum Temperature
+{
+    Freeze,
+    Low,
+    Warm,
+    Boil
+}
+
+struct Customers
+{
+    private string name;
+    private double balance;
+    private int id;
+
+    public void createCust(string n, double b, int i)
+    {
+        name = n;
+        balance = b;
+        id = i;
+    }
+
+    public void showCust()
+    {
+        Console.WriteLine("Name : " + name);
+        Console.WriteLine("Balance : " + balance);
+        Console.WriteLine("ID : " + id);
+    }
+}
+
+
+
+
 namespace c_sharp_drill_23_sectioned
 {
     class Program
@@ -57,6 +91,67 @@ namespace c_sharp_drill_23_sectioned
             Console.WriteLine(try2.OverRideMethod("tasefffdgsdf"));
             Console.ReadLine();
 
+            Temperature micTemp = Temperature.Low;
+
+            Customers bob = new Customers();
+
+            bob.createCust("Bob", 15.50, 12345);
+
+            bob.showCust();
+
+            int[] randArray = new int[5];
+
+            //nullable type?
+            int nullVar = randArray[4];
         }
+
+        abstract class Shape
+        {
+            public abstract double area();
+
+            // An abstract class can contain complete or default code for methods
+            public void sayHi()
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+
+        // A class can have many interfaces
+        // An interface can't have concrete code
+        public interface ShapeItem
+        {
+            double area();
+        }
+
+        class Rectangle : Shape
+        {
+            private double length;
+            private double width;
+
+            public Rectangle(double num1, double num2)
+            {
+                length = num1;
+                width = num2;
+            }
+
+            public override double area()
+            {
+                return length * width;
+            }
+
+            // You can redefine many built in operators so that you can define what happens when you 
+            // add to Rectangles
+            public static Rectangle operator +(Rectangle rect1, Rectangle rect2)
+            {
+                double rectLength = rect1.length + rect2.length;
+                double rectWidth = rect1.width + rect2.width;
+
+                return new Rectangle(rectLength, rectWidth);
+
+            }
+
+        }
+
     }
 }
+
